c69dd550df3f6ba510d038ae52ec59a87cfa8336
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-27 16:40:53 +0600

    prompt alert confirm

---
+++ oddeven.js
@@ -0,0 +1,15 @@
+const userInput = window.prompt('Enter Number')
+
+
+function checkEvenOdd (userInput){
+    if(userInput %2 == 0){
+        return ('Even Number');
+    }
+    return ('Odd Number');
+}
+
+/* 
+ var isMyNumberEven = checkEvenOdd(2222);
+console.log('My Number Is', isMyNumberEven) */
+
+console.log(checkEvenOdd())
